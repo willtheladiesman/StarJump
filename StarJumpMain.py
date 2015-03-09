8dc041e1873afd3b7222ab0e3379225afe1d2eae
willtheladiesman <williamoswald99@gmail.com>
2015-03-09 18:12:43 -0300

    v.1.2 Will

--- StarJumpMain.py
+++ StarJumpMain.py
@@ -5,3 +5,7 @@ import random
 #this is the start of a new game
 #For now we have nothing but soon....
 #We will have the world!!!
+#This code is written in python and pygame
+#When publishing new bits of code please put the version and your name
+#the scrip editor that I use and I personally think is the best is called Sublime Script (v.3)
+
