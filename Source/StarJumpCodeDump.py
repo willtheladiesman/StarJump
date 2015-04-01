7ede187e7b26bb77f5edb691bac36c2456606000
willtheladiesman <williamoswald99@gmail.com>
2015-03-31 21:00:24 -0300

    added code dump

---
+++ Source/StarJumpCodeDump.py
@@ -0,0 +1,7 @@
+#put all code bits here before adding them to StarJumpMain.py
+
+#Divide all code bits by using "#"
+
+#Devs will say if code bit approved to be intergrated
+
+#THANK YOU VERY MUCH!!
